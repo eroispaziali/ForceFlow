de49bcf6bfddc4d67d977afe5c5e9b39fdf50bd2
Lorenzo Frattini <lorenzo.frattini@gmail.com>
2016-09-26 22:29:38 +0100

    API upgrade to version 37

--- src/main/java/com/spaceheroes/task/RunApexTask.java
+++ src/main/java/com/spaceheroes/task/RunApexTask.java
@@ -43,4 +43,8 @@ public class RunApexTask extends SalesforceTask {
 		addText(new Scanner(sourceFile, "UTF-8" ).useDelimiter("\\A").next());
 	}
 
+	public final String getApexCode() {
+		return apexCode;
+	}
+
 }
