c3a9b64fe8b5f7e6ff76984885ebf9d3655bbbd6
eroispaziali <lorenzo.frattini@gmail.com>
2016-10-25 17:22:08 +0100

    unit test updates

--- src/test/java/com/spaceheroes/test/FlowUtilsTest.java
+++ src/test/java/com/spaceheroes/test/FlowUtilsTest.java
@@ -4,30 +4,58 @@ import java.io.File;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
 
 import com.spaceheroes.util.FlowUtils;
+import com.spaceheroes.xml.sfdc.Manifest;
+
 
 public class FlowUtilsTest {
 	
 	@Test
 	public void testFlowPackage() throws Exception {
-		FlowUtils.createRetrieveAllFlowsManifest("target/test-tmp/flow-retrieve");
+		String destinationPath = "target/test-tmp/flow-retrieve";
+		FlowUtils.createRetrieveAllFlowsManifest(destinationPath);
+		checkThatManifestExists(destinationPath, "package.xml");
 	}
 
 	@Test
 	public void testFlowInactivation() throws Exception {
-		FlowUtils.createFlowInactivation("src/test/resources/src", "target/test-tmp/flow-inactivation", true);
+		String destinationPath = "target/test-tmp/flow-inactivation";
+		FlowUtils.createFlowInactivation("src/test/resources/src", destinationPath, true);
+		checkThatManifestExists(destinationPath, "package.xml");
+		checkThatManifestExists(destinationPath, "destructiveChangesPost.xml");
 	}
 	
 	@Test
 	public void testFlowDeletion() throws Exception {
-		FlowUtils.createFlowDeletionPack("src/test/resources/src", "target/test-tmp/flow-deletion");
+		String destinationPath = "target/test-tmp/flow-deletion";
+		FlowUtils.createFlowDeletionPack("src/test/resources/src", destinationPath);
+		checkThatManifestExists(destinationPath, "destructiveChanges.xml");
 	}
 	
 	@After
 	public void tearDown() {
 		FileUtils.deleteQuietly(new File("target/test-tmp"));
+	}
+	
+	private static void checkThatManifestExists(String path, String filename) {
+		
+		// Make sure that file exists, and it's not empty
+		File file = new File(path + "/" + filename);
+		Assert.assertTrue("Manifest must exist", file.exists());
+		Assert.assertTrue("Manifest must not be empty", file.length()>0);
+		
+		// Make sure that file can be read correctly
+		try {
+			Serializer serializer = new Persister();
+			serializer.read(Manifest.class, file);
+		} catch (Exception e) {
+			Assert.fail("Manifest must be valid and deserializable");
+		}
 	}
 	
 }
